f8291a90a10b0e5bdd12a47d1624337335e01475
gemintangsf <gemintangsfurqon@gmail.com>
2026-04-11 21:21:53 +0700

    :lipstick: change theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import HeroSection from "./components/HeroSection";
 import TechStack from "./components/TechStack";
+import ServicesSection from "./components/ServicesSection";
+import ProjectsSection from "./components/ProjectsSection";
+import AboutSection from "./components/AboutSection";
+import FAQSection from "./components/FAQSection";
+import ContactSection from "./components/ContactSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <HeroSection />
-      <TechStack />
-      {/* nanti konten lain seperti About, Projects, Skills */}
+      <div>
+        <TechStack />
+        <ServicesSection />
+        <ProjectsSection />
+        <AboutSection />
+        <FAQSection />
+        <ContactSection />
+      </div>
     </main>
   );
 }
