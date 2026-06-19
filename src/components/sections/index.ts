b8159ed0f38c0aa7866324eb990c8cff984ebdbb
gemintangsf <gemintangsfurqon@gmail.com>
2026-06-19 22:45:02 +0700

    refactor(clean): remove unused components, pages, and their exports

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,7 +1,4 @@
-export { default as AboutSection } from "./AboutSection";
 export { default as ContactSection } from "./ContactSection";
-export { default as HeroSection } from "./HeroSection";
-export { default as ProjectsSection } from "./ProjectsSection";
+export { default as LandingHero } from "./LandingHero";
 export { default as ProjectsPreviewSection } from "./ProjectsPreviewSection";
-export { default as TechStack } from "./TechStack";
 export { default as JourneySection } from "./JourneySection";
